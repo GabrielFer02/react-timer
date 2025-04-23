06d6b01e324b4fec80d15f5148f6575d1b62a052
Gabriel Fernando <gf313074@gmail.com>
2025-04-22 22:10:58 -0300

    create history page

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -1,3 +1,55 @@
+import style from './History.module.css';
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <main className={style['history-container']}>
+      <h1>History</h1>
+      <div className={style['history-list']}>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <span className={`${style.status} ${style['green']}`}>Concluído</span>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <span className={`${style.status} ${style['yellow']}`}>Em andamento</span>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <span className={`${style.status} ${style['red']}`}>Interrompido</span>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <span className={style.status}>Status</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
 }
